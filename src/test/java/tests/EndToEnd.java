f7072961676ce12d55be688aabaa3620a463abfd
abhishekSharma000888 <your_email@domain.com>
2026-09-03 13:57:28 +0530

    added tests

---
+++ src/test/java/tests/EndToEnd.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class EndToEnd extends ParentClass {
+
+    @Test
+    public void employeeLifecycle() {
+
+        System.out.println("Employee lifecycle test started");
+
+    }
+}
