d0e6d8ffa8cd2f5dce50be3df09e6f0fb3e6ee25
abhishekSharma000888 <your_email@domain.com>
2026-09-08 14:54:08 +0530

    updated Parent Class

--- src/test/java/tests/ParentClass.java
+++ src/test/java/tests/ParentClass.java
@@ -7,14 +7,24 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utilities.Common;
 import utilities.SelfHealingReport;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import utilities.TestLogAppender;
 
 public class ParentClass {
 
     protected WebDriver driver;
     protected Common common;
+    private TestLogAppender testLogAppender;
+    private Logger rootLogger;
 
     @BeforeMethod
     public void setUp() {
+        rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        testLogAppender = new TestLogAppender();
+        testLogAppender.start();
+        rootLogger.addAppender(testLogAppender);
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/");
@@ -32,17 +42,22 @@ public class ParentClass {
             SelfHealingReport.generateReport(
                     reportPath,
                     testName,
+                    testLogAppender.getLogs(),
                     common.getHealingEvents()
             );
 
             System.out.println("Self-healing report generated: " + reportPath);
-
         } catch (Exception e) {
             System.out.println("Failed to generate self-healing report: " + e.getMessage());
-        }
+        } finally {
+            if (rootLogger != null && testLogAppender != null) {
+                rootLogger.detachAppender(testLogAppender);
+                testLogAppender.stop();
+            }
 
-        if (driver != null) {
-            driver.quit();
+            if (driver != null) {
+                driver.quit();
+            }
         }
     }
 }
