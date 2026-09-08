a67e15aa82e159dce6baa2974134a28c0405e3fb
abhishekSharma000888 <your_email@domain.com>
2026-09-08 14:48:41 +0530

    added test LogAppender

---
+++ src/main/java/utilities/TestLogAppender.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestLogAppender extends AppenderBase<ILoggingEvent> {
+
+    private final List<String> logs = new ArrayList<>();
+
+    @Override
+    protected void append(ILoggingEvent event) {
+        logs.add(event.getFormattedMessage());
+    }
+
+    public List<String> getLogs() {
+        return new ArrayList<>(logs);
+    }
+
+    public void clearLogs() {
+        logs.clear();
+    }
+}
